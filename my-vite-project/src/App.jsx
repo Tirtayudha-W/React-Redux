3cfe69dcb580341e78dabc793ffdb6b777b9f8bc
Tirta Yudha Wibowo <yudhatirtawibowo@gmail.com>
2024-05-13 12:04:22 +0700

    first commit

---
+++ my-vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CounterComponent from './components/CounterComponent';
+import UserComponent from './components/UserComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Redux Example App</h1>
+      <CounterComponent />
+      <UserComponent />
+    </div>
+  );
+}
+
+export default App;
